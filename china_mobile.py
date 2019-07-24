09a92862122fe23f5c0c47a50854a1c6c356ba6a
赖俊元 <laijy@gzsendi.cn>
2019-07-24 14:28:55 +0800

    MODIFY 修复浏览器选项导致无法成功收到验证码BUG

--- china_mobile.py
+++ china_mobile.py
@@ -20,9 +20,11 @@ class DriverForChinaMobile():
         chrome_options=Options()
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument('blink-settings=imagesEnabled=false')
+        # chrome_options.add_argument('blink-settings=imagesEnabled=false')
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--headless')
+        prefs = {"profile.managed_default_content_settings.images": 2}
+        chrome_options.add_experimental_option("prefs", prefs)
         if proxy:
             chrome_options.add_argument("--proxy-server=http://%s" % proxy)
             # print('登录:代理 http://%s' % proxy)
@@ -55,6 +57,7 @@ class DriverForChinaMobile():
         if '达到上限' in msg:
             result['resultCode'], result['msg'] = '2', msg
         self.driver.quit()
+        print(result)
         return result
 
     def get_msg(self, times=1):
